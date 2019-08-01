84e0f6d4baad7905d39d272b70c8e68f98dffc50
llgpol <771902944@qq.com>
2019-08-01 21:05:05 +0800

    commit

---
+++ src/main/java/com/itoken/service/admin/mapper/TbSysUserMapper.java
@@ -0,0 +1,7 @@
+package com.itoken.service.admin.mapper;
+
+import com.itoken.service.admin.domain.TbSysUser;
+import tk.mybatis.mapper.MyMapper;
+
+public interface TbSysUserMapper extends MyMapper<TbSysUser> {
+}
---
+++ src/test/java/com/itoken/service/admin/service/AdminServiceTest.java
@@ -0,0 +1,22 @@
+package com.itoken.service.admin.service;
+
+import com.itoken.service.admin.ServiceAdminApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ServiceAdminApplication.class)
+public class AdminServiceTest {
+
+    @Test
+    public void register() {
+
+    }
+
+    @Test
+    public void login() {
+
+    }
+}
